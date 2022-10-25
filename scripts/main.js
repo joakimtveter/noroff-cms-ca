abc019168a6e1024fa838e2452c8a7a4d7e78316
Joakim Tveter <joakim@tveter.net>
2022-10-25 22:40:08 +0200

    Complete process level 1

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,56 @@
 import { showToast } from './toast.js';
-import { PRODUCTS } from '../backend/db.js';
 
 const currentLocation = window.location.pathname;
+const baseUrl = 'https://gamehubapi.tveter.one';
 const endpoint = 'https://gamehubapi.tveter.one/wp-json';
+
+const key = 'consumer_key=ck_acbfe22617ccc393377964d6299bb54d26e007b6';
+const secretKey = 'consumer_secret=cs_6b0773d3d8f02ea5d81759bbaf3bc88af81f8a31';
+
+async function loadSearchResults() {
+    const searchTerm = getSearchTerm();
+
+    try {
+        const response = await fetch(`${baseUrl}/wp-json/wc/v3/products?search=${searchTerm}&${key}&${secretKey}`);
+        const searchResult = await response.json();
+        uptateSearchPageTitleAndSearchBox(searchTerm);
+        renderSearchResults(searchResult, searchTerm);
+        enableAddToCartButtons();
+        enableWishlistButtons();
+        updateWishlistBadge();
+        productImageEventlistner();
+    } catch (error) {
+        console.error(error);
+        showToast('Error loading search results', 'error');
+    }
+}
 
 async function loadAllGames() {
     try {
-        const response = await fetch(`${endpoint}/wc/store/products/`);
+        const response = await fetch(`${baseUrl}/wp-json/wc/v3/products?${key}&${secretKey}`);
         const games = await response.json();
         renderGames(games);
         enableAddToCartButtons();
         enableWishlistButtons();
+        updateWishlistBadge();
+        productImageEventlistner();
+    } catch (error) {
+        showToast('Failed to fetch games', 'error');
+        console.error(error);
+    }
+}
+
+async function loadFeatuedGames() {
+    try {
+        const response = await fetch(`${baseUrl}/wp-json/wc/v3/products?${key}&${secretKey}`);
+        const games = await response.json();
+        const featuredGames = games.filter((game) => game.featured);
+        featuredGames.length = 4;
+        renderFeaturedGames(featuredGames);
+        enableAddToCartButtons();
+        enableWishlistButtons();
+        updateWishlistBadge();
+        productImageEventlistner();
     } catch (error) {
         showToast('Failed to fetch games', 'error');
         console.error(error);
@@ -24,19 +64,23 @@ async function loadSingleGame() {
 
     // Return to product list if no id is provided
     if (!params.gameid) {
+        console.error('No game id provided');
         window.location.replace('/games.html');
         return;
     }
 
     // Get single game from API
     try {
-        const response = await fetch(`${endpoint}/wc/store/products/${params.gameid}`);
+        const response = await fetch(`${baseUrl}/wp-json/wc/v3/products/${params.gameid}?${key}&${secretKey}`);
         const game = await response.json();
         renderSingleGame(game);
+        enableAddToCartButtons();
+        enableWishlistButtons();
+        updateWishlistBadge();
     } catch (error) {
         showToast('Failed to fetch games', 'error');
         console.error(error);
-        window.location.replace('/404.html');
+        // window.location.replace('/404.html');
     }
 }
 
@@ -55,6 +99,7 @@ async function loadWishlist() {
         renderWishlist(wishlistGames);
         enableAddToCartButtons();
         enableRemoveFromWishlistButtons();
+        productImageEventlistner();
     } catch (error) {
         showToast('Failed to fetch games', 'error');
         console.error(error);
@@ -87,6 +132,8 @@ async function loadCart() {
             return false;
         });
         renderCartItems(cartGames);
+        enableRemoveFromCartButtons();
+        productImageEventlistner();
     } catch (error) {
         showToast('Failed to fetch games', 'error');
         console.error(error);
@@ -164,6 +211,50 @@ function handleImageClicks(e) {
 }
 
 // RENDER FUNCTIONS
+function renderFeaturedGames(games) {
+    const featuredGamesContainer = document.querySelector('.featured-games');
+    let featuredGames = '';
+    games.forEach((game) => {
+        featuredGames += `
+        <li class="product__card">
+            <img
+                class="product__image"
+                src="${game.images[0].src}"
+                alt="${game.name} cover"
+                data-gameid="${game.id}"
+            />
+            <div class="product__header">
+                <a href="/game.html?gameid=${game.id}"> <h3 class="product__title">${game.name}</h3></a>
+                <button class="wishlist-btn sm" data-active="false" data-gameid="${game.id}">
+                    <svg
+                        class="wishlist-toggle"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 512.05 448.77"
+                    >
+                        <path
+                            class="wishlist-toggle-full"
+                            d="M458.35,65c-57.8-48.6-147.1-41.3-202.4,15-55.3-56.3-144.6-63.7-202.4-15-75.2,63.3-64.2,166.5-10.6,221.2l175.4,178.7a52.52,52.52,0,0,0,75.2.1L469,286.25C522.45,231.55,533.65,128.35,458.35,65Z"
+                            transform="translate(0.05 -31.98)"
+                        />
+                        <path
+                            class="wishlist-toggle-line"
+                            d="M458.4,64.3C400.6,15.7,311.3,23,256,79.3,200.7,23,111.4,15.6,53.6,64.3-21.6,127.6-10.6,230.8,43,285.5L218.4,464.2a52.52,52.52,0,0,0,75.2.1L469,285.6C522.5,230.9,533.7,127.7,458.4,64.3ZM434.8,251.8,259.4,430.5c-2.4,2.4-4.4,2.4-6.8,0L77.2,251.8c-36.5-37.2-43.9-107.6,7.3-150.7,38.9-32.7,98.9-27.8,136.5,10.5l35,35.7,35-35.7c37.8-38.5,97.8-43.2,136.5-10.6,51.1,43.1,43.5,113.9,7.3,150.8Z"
+                            transform="translate(0.05 -31.98)"
+                        />
+                    </svg>
+                    <span class="sr-only">Add to wishlist</span>
+                </button>
+            </div>
+            <div class="product__footer">
+                <p class="product__price">${game.price} kr</p>
+                <button class="add-to-cart" data-gameid="${game.id}">Add to basket</button>
+            </div>
+        </li>
+        `;
+    });
+    featuredGamesContainer.innerHTML = featuredGames;
+}
+
 function renderCartItems(itemsInCart) {
     const cart = document.getElementById('cart-body');
     let cartTotal = 0;
@@ -179,6 +270,7 @@ function renderCartItems(itemsInCart) {
               src="${item?.images[0]?.src}"
               alt="Cover"
               height="150"
+              data-gameid="${item.id}"
               />
           </td>
           <td class="product-item-cell">
@@ -267,30 +359,30 @@ function renderWishlist(wishlist) {
     wishlistContainer.innerHTML = wishlistItems;
 }
 
-function uptateSearchPageTitleAndSearchBox() {
-    const term = window.location.search.substring(3).replace(/\+/g, ' ');
-    document.title = `Search results for ${term} | Gamehub - The universe of games`;
-    document.querySelector('#searchbox').setAttribute('value', term);
+function getSearchTerm() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const searchTerm = urlParams.get('q');
+    return searchTerm;
 }
 
-function renderSearchResults() {
-    const term = window.location.search.substring(3).replace(/\+/g, ' ');
-    const searchResultsContainer = document.getElementById('search-results');
-    const searchResults = PRODUCTS.filter(function (game) {
-        return game.title.toLowerCase().indexOf(term.toLowerCase()) !== -1;
-    });
+function uptateSearchPageTitleAndSearchBox(searchTerm) {
+    document.title = `Search results for ${searchTerm} | Gamehub - The universe of games`;
+    document.querySelector('#searchbox').setAttribute('value', searchTerm);
+}
 
-    let searchResultsHTML = `<p>Your search for "${term}" gave ${searchResults.length} results...</p>`;
-    if (searchResults.length > 0) {
+function renderSearchResults(searchResult, term) {
+    const searchResultsContainer = document.getElementById('search-results');
+    let searchResultsHTML = `<p>Your search for "${term}" gave ${searchResult.length} results...</p>`;
+    if (searchResult.length > 0) {
         searchResultsHTML += '<ul class="search-results">';
-        searchResults.map((result) => {
+        searchResult.map((result) => {
             searchResultsHTML += `
                 <li>
                     <div class="card-large">
-                        <img class="product-image" src="${result.image}" alt="${result.title} cover" />
+                        <img class="product-image" src="${result.images[0].src}" alt="${result.name} cover" />
                         <div class="card-body">
                         <div class="flex flex-center">
-                        <a href="/game.html?${result.id}"><h2 class="title">${result.title}</h2></a>
+                        <a href="/game.html?${result.id}"><h2 class="title">${result.name}</h2></a>
                         <button class="wishlist-btn sm" data-active="false" data-gameid="${result.id}">
                             <svg class="wishlist-toggle" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512.05 448.77" >
                                 <path class="wishlist-toggle-full" d="M458.35,65c-57.8-48.6-147.1-41.3-202.4,15-55.3-56.3-144.6-63.7-202.4-15-75.2,63.3-64.2,166.5-10.6,221.2l175.4,178.7a52.52,52.52,0,0,0,75.2.1L469,286.25C522.45,231.55,533.65,128.35,458.35,65Z" transform="translate(0.05 -31.98)" />
@@ -300,26 +392,26 @@ function renderSearchResults() {
                         </button>
                         </div>
                         <div class="reviews">
-                            <div class="review-container">
-                                <div class="rating-stars" style="--rating: ${result.rating}" role="tooltip" aria-label="${result.title} has a rating of ${result.rating} out of 5."></div>
-                                <div class="rating">${result.rating}</div>
+                            <div class="review-container dark">
+                                <div class="rating-stars" style="--rating: ${result.average_rating}" role="tooltip" aria-label="${result.name} has a rating of ${result.average_rating} out of 5."></div>
+                                <div class="rating">${result.average_rating}</div>
                             </div>
                             <a href="/review.html?${result.id}">Write a review</a>
                         </div>
-                            <p>${result.desciption}</p>
+                            <p>${result.description}</p>
                         </div>
                         <div class="buy-product">
                         <div class="price-stock">
                             <p class="price">${result.price} kr</p>`;
             let indicator = '';
-            if (result.stock < 3 && result.stock != 0) {
+            if (result.stock_quantity < 3 && result.stock_quantity != 0) {
                 indicator = 'warning';
             }
-            if (result.stock <= 0) {
+            if (result.stock_quantity <= 0) {
                 indicator = 'danger';
             }
             searchResultsHTML += `
-                        <p class="stock"><i class="fas fa-box stock-indicator${indicator}"></i> ${result.stock} in stock</p>
+                        <p class="stock"><i class="fas fa-box stock-indicator ${indicator}"></i> ${result.stock_quantity} in stock</p>
                             </div>
                             <button class="btn primary add-to-cart" data-gameid="${result.id}">Add to cart</button>
                         </div>
@@ -356,7 +448,7 @@ function renderGames(games) {
                 </button>
             </div>
             <div class="product__footer">
-                <p class="product__price">${game.prices.price} ${game.prices.currency_symbol}</p>
+                <p class="product__price">${game.price} kr</p>
                 <button class="add-to-cart" data-gameid="${game.id}">
                 Add to basket
                 </button>
@@ -368,8 +460,18 @@ function renderGames(games) {
 
 function renderSingleGame(game) {
     document.title = `${game.name} | Gamehub - The universe of games`;
-
     const gameContainer = document.querySelector('.single-game');
+
+    let ageRating = 'Not listed';
+    if (game.attributes.find((attr) => attr.name === 'AgeRating')) {
+        ageRating = game?.attributes?.find((attr) => attr.name === 'AgeRating').options[0];
+    }
+
+    let multiplayer = 'Not listed';
+    if (game.attributes.find((attr) => attr.name === 'Multiplayer')) {
+        multiplayer = game.attributes.find((attr) => attr.name === 'Multiplayer').options.join(', ');
+    }
+
     let gameHTML = `
         <div class="product-image">
             <img src="${game.images[0].src}" alt="${game.name} cover">
@@ -395,53 +497,45 @@ function renderSingleGame(game) {
         <p>${game.description}</p>
         <div class="buy-product">
             <div class="price-stock">
-            <p class="price">${game.prices.price} ${game.prices.currency_symbol}</p>
+            <p class="price">${game.price} kr</p>
     `;
     let indicator = '';
-    if (game.add_to_cart.maximum < 3 && game.add_to_cart.maximum != 0) {
+    if (game.stock_quantity < 3 && game.stock_quantity != 0) {
         indicator = 'warning';
     }
-    if (game.add_to_cart.maximum <= 0) {
+    if (game.stock_quantity <= 0) {
         indicator = 'danger';
     }
     gameHTML += `
-            <p class="stock"><i class="fas fa-box stock-indicator${indicator}"></i> ${game.add_to_cart.maximum} in stock</p>
+            <p class="stock"><i class="fas fa-box stock-indicator ${indicator}"></i> ${game.stock_quantity} in stock</p>
         </div>
         <button class="btn primary add-to-cart" data-gameid="${game.id}">Add to cart</button>
             </div> <!-- end buy-product-->
             <div class="game-info">
             <table>
-                <tr>
-                <th>Platform:</th>
-                    <td>
-    `;
-    // game?.platform?.map((platform) => (gameHTML += `<li>${platform} </li>`));
-    gameHTML += `
-            </td>
-        </tr>
         <tr>
         <th>Multiplayer:</th>
-        <td>${game.multiplayer}</td>
+        <td>${multiplayer}</td>
         </tr>
         <tr>
         <th>Age Rating:</th>
-        <td>${game.ageRating}</td>
+        <td> ${ageRating} </td>
         </tr>
         <tr>
         <th>Genre:</th>
             <td>
-    ${game.categories.map((category) => `<span class="tag">${category.name}</span>`).join(', ')}
+    ${game.categories.map((category) => `<span class="tag">${category.name}</span>`).join(', ') || 'Not listed'}
         </td>
     </tr>
     <tr>
       <th>Tags:</th>
       <td>
-        ${game.tags.map((tag) => `<span class="tag">${tag.name}</span>`).join(', ')}
+        ${game.tags.map((tag) => `<span class="tag">${tag.name}</span>`).join(', ') || 'Not listed'}
       </td>
         </tr>
         <tr>
         <th>Relase date:</th>
-        <td>${game.relaseDate}</td>
+        <td>${new Date(game.date_created).toLocaleDateString() || 'Not listed'}</td>
         </tr>
     </table>
     </div> <!-- end game info -->
@@ -561,6 +655,9 @@ function closeMenu() {
 }
 
 switch (currentLocation) {
+    case '/':
+        loadFeatuedGames();
+        break;
     case '/basket.html':
         loadCart();
         break;
@@ -568,8 +665,7 @@ switch (currentLocation) {
         loadWishlist();
         break;
     case '/search.html':
-        uptateSearchPageTitleAndSearchBox();
-        renderSearchResults();
+        loadSearchResults();
         break;
     case '/game.html':
         loadSingleGame();
@@ -579,24 +675,6 @@ switch (currentLocation) {
         break;
 }
 
-// INVOKE THE PAGE RELATED FUNCTIONS
-// if (currentLocation === '/basket.html') {
-//     renderCartItems();
-//     enableRemoveFromCartButtons();
-// }
-// if (currentLocation === '/wishlist.html') {
-//     renderWishlistItems();
-// }
-// if (currentLocation === '/search.html') {
-//     uptateSearchPageTitleAndSearchBox();
-//     renderSearchResults();
-// }
-// if (currentLocation === '/games.html') {
-//     loadAllGames();
-// }
-// if (currentLocation === '/game.html') {
-//     loadSingleGame();
-// }
 enableMenuButtons();
 enableAddToCartButtons();
 enableWishlistButtons();
